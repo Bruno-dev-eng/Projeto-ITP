bac6be0b98fc560a15795bb971a4ab3bf12b49d8
Mattno777 <matheus.hgsguedes@gmail.com>
2026-06-18 19:54:45 -0300

    Refactor Imagem class with new attributes and methods

--- Matheus/Imagem.h
+++ Matheus/Imagem.h
@@ -1,17 +1,25 @@
 #ifndef IMAGEM_H
 #define IMAGEM_H
 
-int height_y;
-int width_x;
+#include "Cor.h"
+#include <string>
+
+using Pixel = Cor;
 
 class Imagem {
-    
-    public:
-    int linha;
-    int coluna;
+public:
+    int largura;
+    int altura;
+    Pixel** pixels;
 
-    Imagem();
-    Imagem(int linha, int coluna);
+    Imagem(int largura, int altura);
+    ~Imagem();
+
+    Pixel getPixel(int linha, int coluna);
+    void setPixel(int linha, int coluna, Pixel cor);
+
+    void lerPPM(const std::string& nomeArquivo);
+    void salvarPPM(const std::string& nomeArquivo);
 };
 
 #endif
