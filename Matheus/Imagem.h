aec572f6f916215b2e5253120451e4b551857cbd
Mattno777 <matheus.hgsguedes@gmail.com>
2026-06-18 10:18:48 -0300

    Add Imagem class with height and width attributes

--- Matheus/Imagem.h
+++ Matheus/Imagem.h
@@ -1,1 +1,17 @@
+#ifndef IMAGEM_H
+#define IMAGEM_H
 
+int height_y;
+int width_x;
+
+class Imagem {
+    
+    public:
+    int linha;
+    int coluna;
+
+    Imagem();
+    Imagem(int linha, int coluna);
+};
+
+#endif
