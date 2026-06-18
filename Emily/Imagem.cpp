6af059c2e9e26a7c2872fbbcc3844eeba79dad30
AtlasGab <emilygsa@gmail.com>
2026-06-17 22:29:14 -0300

    Add files via upload

--- Emily/Imagem.cpp
+++ Emily/Imagem.cpp
@@ -1,15 +1,102 @@
 #include "Imagem.h"
+#include <fstream>
 
 Imagem::Imagem(int largura, int altura) {
-    largura = largura;
-    altura = altura;
-    pixels = new Pixel*[altura];
+    this->largura = largura;
+    this->altura = altura;
 
-    for (int i = 0; i < altura; i++) {
-        pixels[i] = new Pixel[largura];
+    pixels = new Pixel*[this->altura];
+
+    for (int linha = 0; linha < this->altura; linha++) {
+        pixels[linha] = new Pixel[this->largura];
     }
 }
 
-imagem::~Imagem() {
-    
+Imagem::~Imagem() {
+    for (int linha = 0; linha < this->altura; linha++) {
+        delete[] pixels[linha];
+    }
+
+    delete[] pixels;
+}
+
+Pixel Imagem::getPixel(int linha, int coluna) {
+    return pixels[linha][coluna];
+}
+
+void Imagem::setPixel(int linha, int coluna, Pixel cor) {
+    pixels[linha][coluna] = cor;
+}
+
+void Imagem::lerPPM(const std::string& nomeArquivo) {
+    std::ifstream arquivo(nomeArquivo);
+
+    if (!arquivo.is_open()) {
+        return;
+    }
+
+    std::string formato;
+    int novaLargura;
+    int novaAltura;
+    int maxValor;
+
+    arquivo >> formato;
+    arquivo >> novaLargura >> novaAltura;
+    arquivo >> maxValor;
+
+    if (novaLargura != largura || novaAltura != altura) {
+
+        for (int linha = 0; linha < altura; linha++) {
+            delete[] pixels[linha];
+        }
+
+        delete[] pixels;
+
+        largura = novaLargura;
+        altura = novaAltura;
+
+        pixels = new Pixel*[altura];
+
+        for (int linha = 0; linha < altura; linha++) {
+            pixels[linha] = new Pixel[largura];
+        }
+    }
+
+    for (int linha = 0; linha < altura; linha++) {
+        for (int coluna = 0; coluna < largura; coluna++) {
+            int vermelho;
+            int verde;
+            int azul;
+
+            arquivo >> vermelho >> verde >> azul;
+
+            pixels[linha][coluna] = Cor(vermelho, verde, azul);
+        }
+    }
+
+    arquivo.close();
+}
+
+void Imagem::salvarPPM(const std::string& nomeArquivo) {
+    std::ofstream arquivo(nomeArquivo);
+
+    if (!arquivo.is_open()) {
+        return;
+    }
+
+    arquivo << "P3\n";
+    arquivo << largura << " " << altura << "\n";
+    arquivo << "255\n";
+
+    for (int linha = 0; linha < altura; linha++) {
+        for (int coluna = 0; coluna < largura; coluna++) {
+            arquivo << pixels[linha][coluna].r << " "
+                    << pixels[linha][coluna].g << " "
+                    << pixels[linha][coluna].b << " ";
+        }
+
+        arquivo << "\n";
+    }
+
+    arquivo.close();
 }
--- Emily/Imagem.h
+++ Emily/Imagem.h
@@ -2,17 +2,24 @@
 #define IMAGEM_H
 
 #include "Cor.h"
+#include <string>
 
 using Pixel = Cor;
 
 class Imagem {
-    public:
-        int largura;
-        int altura;
-        Pixel** pixels;
+public:
+    int largura;
+    int altura;
+    Pixel** pixels;
 
-        Imagem(int largura, int altura);
-        ~Imagem();
-}
+    Imagem(int largura, int altura);
+    ~Imagem();
+
+    Pixel getPixel(int linha, int coluna);
+    void setPixel(int linha, int coluna, Pixel cor);
+
+    void lerPPM(const std::string& nomeArquivo);
+    void salvarPPM(const std::string& nomeArquivo);
+};
 
 #endif
